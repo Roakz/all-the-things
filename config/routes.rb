88fd9a6c7ad8513fecc7c11f803b7d20949dd2db
Rory <rory_bell_88@hotmail.com>
2019-10-29 19:14:05 +1000

    item cards styled and category controller created with an index and show item, category index heading card created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
 
+
+# Home page route for shop index pages
 root "shop#index"
 
+#categories index and show pages get routes
+get 'categories/index'
+get 'categories/show'
+
+#  get root set oustide of the resource and before it so that rails finds it first
 get "shop/index", to:"shop#index"
 
+# overiding the devise registration controller path to set my own after registration route
+# shop routes nested within user and item routes nested within shop
 devise_for :users, controllers: { registrations: "registrations" }
 resources :users do
     resources :shop do
@@ -11,6 +20,7 @@ resources :users do
     end
 end
 
+# User profile resource routes
 resources :user_profiles
 
 end
