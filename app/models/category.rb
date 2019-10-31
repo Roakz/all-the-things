953dc1ef94d1abcbca106c6901ebd6420c9e45bb
Rory <rory_bell_88@hotmail.com>
2019-10-31 17:56:37 +1000

    added a dependent destroy to the database to allow seeding

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-    has_many :items_category
-    has_many :items, through: :items_category
+    has_many :items_category, dependent: :destroy
+    has_many :items, through: :items_category, dependent: :destroy
     validates :name, uniqueness: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :item_picture
   belongs_to :shop
-  has_many :items_category
-  has_many :categories, through: :items_category
+  has_many :items_category, dependent: :destroy
+  has_many :categories, through: :items_category, dependent: :destroy
   accepts_nested_attributes_for :categories
 end
