884603cd7ea2848041e5d1609f13b594ac44b9ba
Rory <rory_bell_88@hotmail.com>
2019-11-07 14:28:47 +1000

    corrected key name

--- app/controllers/shop_owners_controller.rb
+++ app/controllers/shop_owners_controller.rb
@@ -4,7 +4,7 @@ class ShopOwnersController < ApplicationController
 
   def new
 
-    Stripe.api_key = Rails.application.credentials.dig(:stripe, :api_test_key)
+    Stripe.api_key = Rails.application.credentials.dig(:stripe, :api_test_secret)
 
     @session = Stripe::Checkout::Session.create(
     payment_method_types: ['card'],
